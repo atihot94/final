cd9c89ea8c1b3b48f06dd75ee848844927045563
atif javaved <atihot94@gmail.com>
2015-12-27 12:46:58 +0500

    this is my first comit for final lib preperation

---
+++ add/src/add/demo.java
@@ -0,0 +1,19 @@
+package add;
+
+import java.util.Scanner;
+
+public class demo {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("enter number 1  :");
+		int a= scanner.nextInt();
+		System.out.println("enter number 2  :");
+		int b=scanner.nextInt();
+	
+		int c=a+b;
+		System.out.println("sum of two numbers are :" + c);
+
+	}
+
+}
