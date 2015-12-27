efb49e33a920b3a923bf1fc43e459282a58bfc4b
atif javaved <atihot94@gmail.com>
2015-12-27 12:58:27 +0500

    division

--- add/src/add/demo.java
+++ add/src/add/demo.java
@@ -13,9 +13,10 @@ public class demo {
 	
 		int c=a+b;
 		int s=a-b;
+		int m=a*b; 
 		System.out.println("sum of two numbers is :" + c);
 		System.out.println("subtrction of two numbers is :" + s);
-
+		System.out.println("multipication of two numbers is :" + m);
 	}
 
 }
